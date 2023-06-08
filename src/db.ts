98acdddf1e5f8c665889c1fc30c745ea71a1d2b5
southernMD <2483723241@qq.com>
2023-06-08 17:43:28 +0800

    mysql

--- src/db.ts
+++ src/db.ts
@@ -1,13 +1,14 @@
 import mysql from "mysql"
  
 let pool = mysql.createPool({
-    host:"localhost",    //主机名
-    user:"root",           //账户
-    password:"m123042012",   //密码
+    host:"5rg9702841.goho.co",    //主机名
+    port:45019,
+    user:"NMuser",           //账户
+    password:"zjitc",   //密码
     database:"music",    //使用的数据表
     multipleStatements: true
 })
- 
+
 //不用修改
 function query(sql: string | mysql.QueryOptions,callback: { (err: any, data: any[]): void; (err: any, data: any): void; (err: any, data: any): void; (err: any, data: any): void; (err: any, data: any): void; (err: any, data: any): void; (arg0: mysql.MysqlError | null, arg1: any): void }){
     pool.getConnection(function(err,coonection){
